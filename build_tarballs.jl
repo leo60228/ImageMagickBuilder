9bc75e56e49f5e0b3fa716cc27fcc8b5083e8239
leo60228 <iakornfeld@gmail.com>
2018-10-18 15:15:46 -0400

    fix platforms

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -35,7 +35,7 @@ exit
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = filter(e -> !(occursin("apple", e) || occursin("darwin", e)), supported_platforms())
+platforms = filter(e -> typeof(e) == Windows || typeof(e) == Linux, supported_platforms())
 
 # The products that we will ensure are always built
 products(prefix) = [
